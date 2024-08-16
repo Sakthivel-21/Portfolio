6f8766213d36a3b4b023844faa2f5a4b5491acbc
Sakthivel-21 <147375017+Sakthivel-21@users.noreply.github.com>
2024-08-16 01:51:08 -0700

    1stcommit

---
+++ Portfolio/index.js
@@ -0,0 +1,60 @@
+var call=document.getElementById("black")
+var llac=document.getElementById("helo")
+
+function nith(){
+    llac.style.display="none"
+}
+
+
+ScrollReveal().reveal('.head-2');
+ScrollReveal().reveal('.head-3', { delay: 200 });
+ScrollReveal().reveal('.head-4', { delay: 500,
+  origin:'bottom'
+ });
+
+ ScrollReveal().reveal('.head-7',{
+  delay:200,
+  origin:'right',
+  distance:'50px'
+ });
+
+ ScrollReveal().reveal('.about-img',{
+  delay:500,
+  origin:'left',
+  distance:'50px'
+ });
+
+ ScrollReveal().reveal('.about-content',{
+  delay:500,
+  origin:'right',
+  distance:'50px'
+ });
+
+ ScrollReveal().reveal('.pho', { delay: 200 });
+ ScrollReveal().reveal('.ne', { delay: 200 });
+
+ ScrollReveal().reveal('.services', { delay: 200 });
+ ScrollReveal().reveal('.services-1', { delay: 300 });
+ ScrollReveal().reveal('.services-3', { delay: 400 });
+
+ ScrollReveal().reveal('.mg', { delay: 400 });
+ ScrollReveal().reveal('.gm', { delay: 400 });
+ ScrollReveal().reveal('.bm', { delay: 400 });
+
+ ScrollReveal().reveal('.next', { delay: 400 });
+ ScrollReveal().reveal('.tab', { delay: 400 });
+ 
+ 
+ ScrollReveal().reveal('.skill-1',{
+  delay:500,
+  origin:'left',
+  distance:'50px'
+ });
+
+ ScrollReveal().reveal('.skill-2',{
+  delay:500,
+  origin:'right',
+  distance:'50px'
+ });
+
+ ScrollReveal().reveal('.container', { delay: 400 });
